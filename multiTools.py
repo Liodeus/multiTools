fbb1f7e52efb32441612616e15124b85b9279c9b
Liodeus <thibaultgalbourdin@live.fr>
2017-06-21 20:18:42 +0200

    rename time, does a few test with selenium and add a menu to launch code

--- multiTools.py
+++ multiTools.py
@@ -1,17 +1,48 @@
 #!usr/bin/env Python
 # -*- coding:utf_8 -*-
 
-# program give:
-#     - weather
-#     - time(datetime)
-#     - ip
-#     - geolocalisation
-
 import sys
+import temps
+import ip
+import weather
 
 
 def main():
-    print("main")
+    print("1. Time")
+    print("2. Ip")
+    print("3. Weather \n")
+
+    choice = input("Choice : ")
+    print()
+
+    if choice == "1":
+        print("1. Time")
+        print("2. Calendar of the month")
+        print("3. Calendar of the year")
+        print("4. Epoch \n")
+
+        scdChoice = input("Choice : ")
+        print()
+
+        if scdChoice == "1":
+            temps.temps()
+        elif scdChoice == "2":
+            temps.calendarOfTheMonth()
+        elif scdChoice == "3":
+            temps.calendarOfTheYear()
+        elif scdChoice == "4":
+            temps.epoch()
+        else:
+            print("Error !")
+
+    elif choice == "2":
+        ip.ipInfo()
+
+    elif choice == "3":
+        weather.weather()
+
+    else:
+        print("Error !")
 
 
 if __name__ == "__main__":
--- weather.py
+++ weather.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env Python
 # -*- coding:utf_8 -*-
 
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import re
+
+from selenium import webdriver
+
+
+# If this doesn't work you need to install geckodriver
+# https://github.com/mozilla/geckodriver
+def weather():
+    browser = webdriver.Firefox()
+    browser.get('https://www.tameteo.com/')
+
+
+if __name__ == "__main__":
+    weather()
